6ae512539ba4b2301e8a53a70ef739ec78c9d94b
Davi Segantin <davi.g.segantin@gmail.com>
2024-11-04 16:53:10 -0300

    implementando paginas de posts

---
+++ src/componentes/PostCard/index.js
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import styles from "./PostCard.module.css";
+
+export default function PostCard({ post }) {
+  return (
+    <Link to={`/posts/${post.id}`}>
+      <div className={styles.post}>
+        <img
+          src={`assets/posts/${post.id}/capa.png`}
+          alt="imagem de capa do post"
+          className={styles.capa}
+        />
+        <h2 className={styles.titulo}>{post.titulo}</h2>
+        <button className={styles.botaoLer}>Ler</button>
+      </div>
+    </Link>
+  );
+}
