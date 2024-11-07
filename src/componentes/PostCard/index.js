9379105837665459100e27d9b5344420e4cff087
Davi Segantin <davi.g.segantin@gmail.com>
2024-11-07 12:09:44 -0300

    implementando posts recomendados

--- src/componentes/PostCard/index.js
+++ src/componentes/PostCard/index.js
@@ -1,17 +1,18 @@
 import { Link } from "react-router-dom";
 import styles from "./PostCard.module.css";
+import BotaoPrincipal from "componentes/BotaoPrincipal";
 
 export default function PostCard({ post }) {
   return (
     <Link to={`/posts/${post.id}`}>
       <div className={styles.post}>
         <img
-          src={`assets/posts/${post.id}/capa.png`}
+          src={`/assets/posts/${post.id}/capa.png`}
           alt="imagem de capa do post"
           className={styles.capa}
         />
         <h2 className={styles.titulo}>{post.titulo}</h2>
-        <button className={styles.botaoLer}>Ler</button>
+        <BotaoPrincipal>Ler</BotaoPrincipal>
       </div>
     </Link>
   );
